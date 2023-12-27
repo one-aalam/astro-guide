3ba7eff9959e257d8f4b4c0cb42df0bc3ab4dfea
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2023-12-27 21:45:30 +0530

    feat: add `RadialProgress` component

---
+++ src/components/RadialProgress/config.ts
@@ -0,0 +1,19 @@
+import { type RadialProgressProps } from "./types";
+export const defaultOptions: RadialProgressProps = {
+    colorSlice: "#00a1ff",
+    fontColor: "#000",
+    fontSize: "1.6rem",
+    fontWeight: 400,
+    lineargradient: ["yellow", "#ff0000"],
+    number: true,
+    round: true,
+    fill: "none",
+    unit: "%",
+    rotation: -90,
+    size: 200,
+    stroke: 10,
+    index: 0,
+    inverse: true,
+    percent: 75,
+    // strokeDasharray: "1, 2"
+};
